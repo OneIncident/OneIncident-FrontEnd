6ebdee2a8a46631f56f4618b6c235788dbf0062c
Garnett Grant <net_grant@hotmail.com>
2022-12-15 11:16:23 -0500

    change base url front end

--- client/server.js
+++ client/server.js
@@ -1,8 +1,8 @@
-// const app = require("../server/config/app");
+const app = require("../server/config/app");
 
-// let port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 
-// app.use(express.static('public'));
-// app.get('*',(req, res)=>{
-//     res.sendFile(path.join(__dirname,'public/index.html'));
-// })
+app.use(express.static('public'));
+app.get('*',(req, res)=>{
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
